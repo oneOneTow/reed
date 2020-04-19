9ba50fb72e6b2bad8a1234c3ffad5fd41fa76124
jingguoshuai <jgs@ccbscf.com>
2020-04-19 22:18:05 +0800

    feat:change some code style

--- src/main/java/com/think/reed/rpc/invoke/DefaultReedRpcInvokeFuture.java
+++ src/main/java/com/think/reed/rpc/invoke/DefaultReedRpcInvokeFuture.java
@@ -1,11 +1,13 @@
 package com.think.reed.rpc.invoke;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.CountDownLatch;
+
 import com.think.reed.protocol.ProtocolSign;
 import com.think.reed.rpc.remoting.ReedRemotingCommand;
 import com.think.reed.rpc.remoting.RemotingCommandFactory;
+
 import io.netty.util.Timeout;
-import java.net.InetSocketAddress;
-import java.util.concurrent.CountDownLatch;
 
 /**
  * @author jgs
@@ -13,44 +15,42 @@ import java.util.concurrent.CountDownLatch;
  */
 public class DefaultReedRpcInvokeFuture implements ReedRpcInvokeFuture {
 
-  private CountDownLatch countDownLatch = new CountDownLatch(1);
-  private String invokeId;
-  private ProtocolSign protocol;
-  private RemotingCommandFactory commandFactory;
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+    private String invokeId;
+    private ProtocolSign protocol;
+    private RemotingCommandFactory commandFactory;
 
-  public DefaultReedRpcInvokeFuture(String invokeId, ProtocolSign protocol,
-      RemotingCommandFactory commandFactory) {
-    this.invokeId = invokeId;
-    this.commandFactory = commandFactory;
-    this.protocol = protocol;
-  }
+    public DefaultReedRpcInvokeFuture(String invokeId, ProtocolSign protocol, RemotingCommandFactory commandFactory) {
+        this.invokeId = invokeId;
+        this.commandFactory = commandFactory;
+        this.protocol = protocol;
+    }
 
+    public InetSocketAddress getRemoteAddr() {
+        return null;
+    }
 
-  public InetSocketAddress getRemoteAddr() {
-    return null;
-  }
+    public String getInvokeId() {
+        return null;
+    }
 
-  public String getInvokeId() {
-    return null;
-  }
+    public void addTimeout(Timeout timeout) {
 
-  public void addTimeout(Timeout timeout) {
+    }
 
-  }
+    public void cancelTimeout() {
 
-  public void cancelTimeout() {
+    }
 
-  }
+    public void putResponse(ReedRemotingCommand remotingCommand) {
 
-  public void putResponse(ReedRemotingCommand remotingCommand) {
+    }
 
-  }
+    public void tryAsyncExecuteInvokeCallbackAbnormally() {
 
-  public void tryAsyncExecuteInvokeCallbackAbnormally() {
+    }
 
-  }
-
-  public ReedRemotingCommand createConnectionClosedResponse(InetSocketAddress responseHost) {
-    return null;
-  }
+    public ReedRemotingCommand createConnectionClosedResponse(InetSocketAddress responseHost) {
+        return null;
+    }
 }
