b86942345096b6791922720096417a4b6c03e205
luzhiqing <2319687449@qq.com>
2020-04-19 22:32:07 +0800

    feat: add StringUtils

---
+++ src/main/java/com/think/reed/util/StringUtils.java
@@ -0,0 +1,20 @@
+package com.think.reed.util;
+
+/**
+ * @desc:
+ * @author: zhiqing.lu
+ * @version: v1.0.0
+ * @date: 2020/4/19 18:30
+ **/
+public class StringUtils {
+    /**
+     * 判断是不是空白字符串
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isBlank(String str) {
+        //TODO 引用不同的包实现功能
+        return true;
+    }
+}
