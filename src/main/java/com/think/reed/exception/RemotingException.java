c82a43ccccab877406e9e40fab60413bb371c47a
luzhiqing <2319687449@qq.com>
2020-05-05 23:06:48 +0800

    feat: add remoting exception

---
+++ src/main/java/com/think/reed/exception/RemotingException.java
@@ -0,0 +1,30 @@
+package com.think.reed.exception;
+
+import org.apache.commons.lang.exception.NestableRuntimeException;
+
+/**
+ * @author zhiqing.lu
+ * @version v1.0.0
+ * @date 2020/5/5 01:29
+ **/
+public class RemotingException extends NestableRuntimeException {
+    public RemotingException(String errorCode){
+        super(errorCode);
+    }
+
+    public RemotingException(String errorCode, Throwable cause){
+        super(errorCode, cause);
+    }
+
+    public RemotingException(String errorCode, String errorDesc){
+        super(errorCode + ":" + errorDesc);
+    }
+
+    public RemotingException(String errorCode, String errorDesc, Throwable cause){
+        super(errorCode + ":" + errorDesc, cause);
+    }
+
+    public RemotingException(Throwable cause){
+        super(cause);
+    }
+}
